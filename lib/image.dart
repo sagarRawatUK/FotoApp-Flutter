987a24b5890d5b249e937cdf257603196c836d76
Sagar Rawat <sagarrawatuk@gmail.com>
2020-10-04 10:26:21 +0530

    donwload feature added

--- lib/image.dart
+++ lib/image.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:FotoApp/main.dart';
 
 class ImagePath extends StatelessWidget {
   final String imgPath;
   ImagePath(this.imgPath);
+  Future<String> get localPath async {
+    final directory = await getApplicationDocumentsDirectory();
+    print(directory.path);
+    return directory.path;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: myColor,
+        leading: IconButton(
+          icon: Icon(Icons.close),
+          onPressed: () => Navigator.of(context).pop(),
+          color: Colors.black,
+        ),
+        actions: [
+          IconButton(
+              color: Colors.black,
+              icon: Icon(Icons.file_download),
+              onPressed: () async => DownloadTask(
+                  taskId: await FlutterDownloader.enqueue(
+                      url: imgPath,
+                      savedDir: await localPath,
+                      openFileFromNotification: true,
+                      showNotification: true)))
+        ],
+      ),
       body: SizedBox.expand(
         child: Container(
           child: Stack(
@@ -15,24 +42,6 @@ class ImagePath extends StatelessWidget {
                 alignment: Alignment.center,
                 child: Hero(tag: imgPath, child: Image.network(imgPath)),
               ),
-              Align(
-                alignment: Alignment.topCenter,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  mainAxisSize: MainAxisSize.min,
-                  children: [
-                    AppBar(
-                      elevation: 8.0,
-                      backgroundColor: Colors.transparent,
-                      leading: IconButton(
-                        icon: Icon(Icons.close),
-                        onPressed: () => Navigator.of(context).pop(),
-                        color: Colors.black,
-                      ),
-                    )
-                  ],
-                ),
-              )
             ],
           ),
         ),
--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,15 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+
+final Color myColor = Colors.blue;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterDownloader.initialize(debug: true);
   runApp(
     MyApp(),
   );
